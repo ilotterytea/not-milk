c955a54004fb3d1cdc9fc113a1e80a5ea12e9cc9
ilotterytea <ilotterytea@proton.me>
2023-03-04 05:38:47 +0600

    Response for processed sip

--- src/responses.rs
+++ src/responses.rs
@@ -6,6 +6,8 @@ use rocket::{
 };
 use serde::{Deserialize, Serialize};
 
+use crate::models::User;
+
 #[derive(Serialize)]
 pub struct GenericResponse<T> {
     pub status: u32,
@@ -16,6 +18,14 @@ pub struct GenericResponse<T> {
 pub struct SipRequest {
     pub alias_id: i32,
     pub platform: i32,
+}
+
+#[derive(Serialize)]
+pub struct SipProcessed {
+    pub delay: i32,
+    pub message: String,
+    pub income: i32,
+    pub user: User,
 }
 
 pub enum SipRequestError {
