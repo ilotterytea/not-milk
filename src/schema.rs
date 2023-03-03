f0018d0aa0ac641370f98f8ca111785f438741d7
ilotterytea <ilotterytea@proton.me>
2023-03-03 21:24:17 +0600

    Diesel migrations

---
+++ src/schema.rs
@@ -0,0 +1,13 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Integer,
+        alias_id -> Text,
+        platform -> Integer,
+        points -> Integer,
+        inventory -> Nullable<Text>,
+        created_timestamp -> Integer,
+        last_timestamp -> Integer,
+    }
+}
