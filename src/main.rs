39ccc7d6a1f48d0851d1daedcc2b3f0ad1d71746
ilotterytea <ilotterytea@proton.me>
2023-03-03 21:50:40 +0600

    Establish DB connection function

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,24 @@
 #[macro_use]
 extern crate rocket;
 
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+mod models;
+mod schema;
+
 fn main() {
     println!("Hello, world!");
 
     rocket::ignite().launch();
+}
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
