05f5f4aac1d6d305873aa80bb35336b9d398ed35
ilotterytea <ilotterytea@proton.me>
2023-03-03 23:08:49 +0600

    insertable new user

--- src/models.rs
+++ src/models.rs
@@ -1,13 +1,23 @@
 use crate::schema::users;
 use diesel::prelude::*;
+use serde::Serialize;
 
-#[derive(Queryable)]
+#[derive(Queryable, Serialize)]
 pub struct User {
     pub id: i32,
     pub alias_id: String,
     pub platform: i32,
     pub points: i32,
     pub inventory: Option<String>,
+    pub created_timestamp: i32,
+    pub last_timestamp: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub alias_id: &'a str,
+    pub platform: i32,
     pub created_timestamp: i32,
     pub last_timestamp: i32,
 }
