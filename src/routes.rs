aeb613b4bd123b2a1a6c6edb65e3b42f7f83fe18
ilotterytea <ilotterytea@proton.me>
2023-03-08 21:21:50 +0600

    Added random events.

--- src/routes.rs
+++ src/routes.rs
@@ -1,8 +1,11 @@
 use diesel::{insert_into, update, ExpressionMethods, QueryDsl, RunQueryDsl};
 use rand::Rng;
-use rocket::{http::Status, response::status::Custom};
-use rocket_contrib::json::Json;
+use rocket::{http::Status, response::status::Custom, serde::json::Json};
 use std::{env, fs::File};
+use twitch_api::{
+    twitch_oauth2::{AccessToken, UserToken},
+    TwitchClient,
+};
 
 use crate::{
     establish_connection,
@@ -11,8 +14,10 @@ use crate::{
     structs::Lines,
 };
 
-#[post("/sip", data = "<sip_request>")]
-pub fn take_a_sip_of_tea(sip_request: SipRequest) -> Custom<Json<GenericResponse<SipProcessed>>> {
+#[post("/sip", data = "<data>")]
+pub async fn take_a_sip_of_tea(data: &str) -> Custom<Json<GenericResponse<SipProcessed>>> {
+    let sip_request: SipRequest = serde_json::from_str(data).unwrap();
+
     dotenvy::dotenv().ok();
 
     use crate::schema::users::dsl::*;
@@ -79,7 +84,94 @@ pub fn take_a_sip_of_tea(sip_request: SipRequest) -> Custom<Json<GenericResponse
         .parse::<i32>()
         .unwrap();
 
-    let _points = rand::thread_rng().gen_range(min_per_sip..max_per_sip);
+    let mut _points = rand::thread_rng().gen_range(min_per_sip..max_per_sip);
+
+    let file = File::open("./lines.json").unwrap();
+    let lines: Lines = serde_json::from_reader(file).unwrap();
+
+    let mut percent = (_points as f64 / max_per_sip as f64) * 100.0;
+    let event_id = rand::thread_rng().gen_range(0..2);
+
+    let message = match event_id {
+        1 => {
+            let mut consumers = users
+                .filter(platform.eq(sip_request.platform))
+                .filter(alias_id.ne(sip_request.alias_id.to_string()))
+                .load::<User>(conn)
+                .unwrap_or(Vec::new());
+
+            let index = rand::thread_rng().gen_range(0..consumers.len());
+
+            let mut consumer: &mut User = consumers.get_mut(index).unwrap();
+
+            _points = rand::thread_rng().gen_range(1..max_per_sip);
+            consumer.points -= _points;
+
+            update(users.filter(id.eq(consumer.id)))
+                .set(points.eq(consumer.points))
+                .execute(conn)
+                .expect("Cannot update the values!");
+
+            let client: TwitchClient<reqwest::Client> = TwitchClient::default();
+            let token = match UserToken::from_existing(
+                &reqwest::Client::new(),
+                AccessToken::new(
+                    env::var("ACCESS_TOKEN")
+                        .expect("ACCESS_TOKEN must be set for Twitch API requests!"),
+                ),
+                None,
+                None,
+            )
+            .await
+            {
+                Ok(t) => t,
+                Err(e) => panic!("Couldn't get the token: {}", e),
+            };
+
+            let __id = &consumer.alias_id;
+            let _id = __id.as_str();
+            let ids: &[&twitch_api::types::UserIdRef] = &[_id.into()];
+            let request = twitch_api::helix::users::GetUsersRequest::ids(ids);
+            let res = &client.helix.req_get(request, &token).await.unwrap();
+            let user = res.data.first().unwrap();
+
+            percent = (_points as f64 / max_per_sip as f64) * 100.0;
+
+            format!(
+                "you didn't get your portion of milk, but you {} took {} and pumped all the milk out of him 🥛 🥴 ",
+                if percent >= 60.0 {
+                    "rudely"
+                } else if (25.0..60.0).contains(&percent) {
+                    "gently"
+                } else {
+                    ""
+                },
+                user.login
+            )
+        }
+        _ => if percent >= 90.0 {
+            lines
+                .legendary_lines
+                .get(rand::thread_rng().gen_range(0..lines.legendary_lines.len()))
+                .unwrap()
+        } else if (50.0..90.0).contains(&percent) {
+            lines
+                .epic_lines
+                .get(rand::thread_rng().gen_range(0..lines.epic_lines.len()))
+                .unwrap()
+        } else if (10.0..50.0).contains(&percent) {
+            lines
+                .common_lines
+                .get(rand::thread_rng().gen_range(0..lines.common_lines.len()))
+                .unwrap()
+        } else {
+            lines
+                .poor_lines
+                .get(rand::thread_rng().gen_range(0..lines.poor_lines.len()))
+                .unwrap()
+        }
+        .to_string(),
+    };
 
     _rs.points += _points;
 
@@ -95,33 +187,6 @@ pub fn take_a_sip_of_tea(sip_request: SipRequest) -> Custom<Json<GenericResponse
     .execute(conn)
     .expect("Cannot update the values!");
 
-    let file = File::open("./lines.json").unwrap();
-    let lines: Lines = serde_json::from_reader(file).unwrap();
-
-    let percent = (_points as f64 / max_per_sip as f64) * 100.0;
-
-    let message = if percent >= 90.0 {
-        lines
-            .legendary_lines
-            .get(rand::thread_rng().gen_range(0..lines.legendary_lines.len()))
-            .unwrap()
-    } else if (50.0..90.0).contains(&percent) {
-        lines
-            .epic_lines
-            .get(rand::thread_rng().gen_range(0..lines.epic_lines.len()))
-            .unwrap()
-    } else if (10.0..50.0).contains(&percent) {
-        lines
-            .common_lines
-            .get(rand::thread_rng().gen_range(0..lines.common_lines.len()))
-            .unwrap()
-    } else {
-        lines
-            .poor_lines
-            .get(rand::thread_rng().gen_range(0..lines.poor_lines.len()))
-            .unwrap()
-    };
-
     Custom(
         Status::Ok,
         Json(GenericResponse {
@@ -130,7 +195,7 @@ pub fn take_a_sip_of_tea(sip_request: SipRequest) -> Custom<Json<GenericResponse
                 delay: interval_time
                     - (i32::try_from(chrono::Utc::now().timestamp()).unwrap() - _rs.last_timestamp),
                 income: _points,
-                message: message.clone(),
+                message,
                 user: _rs,
             },
         }),
