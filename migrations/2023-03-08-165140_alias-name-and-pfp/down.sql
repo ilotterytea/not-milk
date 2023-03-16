1cc5818777da16a95e628f740ab8a342f842e8cc
ilotterytea <ilotterytea@proton.me>
2023-03-17 00:40:52 +0600

    initial commit to reimagine

--- migrations/2023-03-08-165140_alias-name-and-pfp/down.sql
+++
@@ -1,6 +0,0 @@
--- This file should undo anything in `up.sql`
-ALTER TABLE Users
-DROP COLUMN alias_name;
-
-ALTER TABLE Users
-DROP COLUMN alias_pfp;
--- migrations/2023-03-08-165140_alias-name-and-pfp/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-ALTER TABLE Users
-ADD COLUMN alias_name TEXT;
-
-ALTER TABLE Users
-ADD COLUMN alias_pfp TEXT;
