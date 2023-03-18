afb5d395580801664f2364a1cfea2edacb3c2ead
ilotterytea <ilotterytea@proton.me>
2023-03-18 17:31:38 +0600

    .round() may do some shit

--- twitch-bot/src/utils.rs
+++ twitch-bot/src/utils.rs
@@ -1,14 +1,14 @@
 pub fn humanize_timestamp_like_timer(timestamp: i32) -> String {
-    let d = (timestamp as f64 / (60.0 * 60.0 * 24.0)).round();
-    let h = (timestamp as f64 / (60.0 * 60.0) % 24.0).round();
-    let m = (timestamp as f64 % (60.0 * 60.0) / 60.0).round();
-    let s = (timestamp as f64 % 60.0).round();
+    let d = (timestamp as f64 / (60.0 * 60.0 * 24.0)) as i32;
+    let h = (timestamp as f64 / (60.0 * 60.0) % 24.0) as i32;
+    let m = (timestamp as f64 % (60.0 * 60.0) / 60.0) as i32;
+    let s = (timestamp as f64 % 60.0) as i32;
 
-    if d == 0.0 && h == 0.0 && m == 0.0 {
+    if d == 0 && h == 0 && m == 0 {
         format!("{}s", s)
-    } else if d == 0.0 && h == 0.0 {
+    } else if d == 0 && h == 0 {
         format!("{}m{}s", m, s)
-    } else if d == 0.0 {
+    } else if d == 0 {
         format!("{}h{}m", h, m)
     } else {
         format!("{}d{}h", d, h)
