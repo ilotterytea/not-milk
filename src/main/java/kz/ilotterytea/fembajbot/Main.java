9d60097e3165269bb8dec15ef736d1c2dac384f2
ilotterytea <ilotterytea@proton.me>
2023-04-16 21:58:59 +0600

    add: init twitch bot

--- src/main/java/kz/ilotterytea/fembajbot/Main.java
+++ src/main/java/kz/ilotterytea/fembajbot/Main.java
@@ -3,5 +3,8 @@ package kz.ilotterytea.fembajbot;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        TwitchBot bot = new TwitchBot();
+        bot.run();
     }
 }
