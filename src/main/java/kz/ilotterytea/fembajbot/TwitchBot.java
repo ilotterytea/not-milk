d73a73a670fde817b588fdd6963148a39d615f0f
ilotterytea <ilotterytea@proton.me>
2023-04-16 20:45:17 +0600

    add: joining "channel" entities chats

--- src/main/java/kz/ilotterytea/fembajbot/TwitchBot.java
+++ src/main/java/kz/ilotterytea/fembajbot/TwitchBot.java
@@ -4,8 +4,15 @@ import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
+import com.github.twitch4j.helix.domain.User;
+import kz.ilotterytea.fembajbot.entities.Channel;
+import kz.ilotterytea.fembajbot.utils.HibernateUtil;
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author ilotterytea
@@ -49,6 +56,25 @@ public class TwitchBot {
         if (credential.getUserName() != null) {
             client.getChat().joinChannel(credential.getUserName());
         }
+
+        // Joining "channel" entities chats:
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Channel> channels = session.createQuery("from Channel where optOutTimestamp is null", Channel.class).getResultList();
+
+        if (!channels.isEmpty()) {
+            List<User> convertedChannels = client.getHelix().getUsers(
+                    null,
+                    channels.stream().map(c -> c.getAliasId().toString()).collect(Collectors.toList()),
+                    null
+            ).execute().getUsers();
+
+            for (User channel : convertedChannels) {
+                client.getChat().joinChannel(channel.getLogin());
+                LOGGER.debug("Joined " + channel.getLogin() + "'s chatroom!");
+            }
+        }
+
+        session.close();
 
         client.getEventManager().onEvent(IRCMessageEvent.class, event -> {
             LOGGER.debug(event.toString());
