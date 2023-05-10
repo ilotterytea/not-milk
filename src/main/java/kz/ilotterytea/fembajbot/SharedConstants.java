09db7fe6030e5e4e99fbf247f73e6db795bac898
ilotterytea <ilotterytea@proton.me>
2023-05-10 15:55:39 +0600

    add: load constants from properties file

--- src/main/java/kz/ilotterytea/fembajbot/SharedConstants.java
+++ src/main/java/kz/ilotterytea/fembajbot/SharedConstants.java
@@ -1,10 +1,32 @@
 package kz.ilotterytea.fembajbot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * @author ilotterytea
  * @version 1.0
  */
 public class SharedConstants {
-    public static final String TWITCH_OAUTH2_TOKEN = System.getProperty("fembajbot.twitch_oauth2_token");
-    public static final String[] DEFAULT_PREFIXES = new String[]{"~", "\uD83E\uDD5B"};
+    public static final String TWITCH_OAUTH2_TOKEN;
+    public static final String[] DEFAULT_PREFIXES;
+
+    static {
+        Properties properties = new Properties();
+
+        Logger logger = LoggerFactory.getLogger(SharedConstants.class.getName());
+
+        try (FileInputStream fis = new FileInputStream(System.getProperty("config_file", "config.properties"))) {
+            properties.load(fis);
+        } catch (IOException e) {
+            logger.error("Couldn't load the properties file: " + e.getMessage());
+        }
+
+        TWITCH_OAUTH2_TOKEN = properties.getProperty("twitch.oauth2_token", null);
+        DEFAULT_PREFIXES = properties.getProperty("default_prefixes", "~,\uD83E\uDD5B").split(",");
+    }
 }
